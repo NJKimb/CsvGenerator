5a1274188d4cf81305ce3067807205ffd605271b
NJKimb <mocilabs@gmail.com>
2024-12-05 23:46:14 -0600

    Initial commit

---
+++ CsvGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CsvGenerator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        String r_line = "";
+        String t_line = "";
+
+        r_line += "doctor, patient\n";
+        t_line += "doctor, age\n";
+
+        for (int i = 0; i < Int32.Parse(args[0]); i++)
+        {
+            r_line += rnd.Next(0, 100).ToString() + "," + rnd.Next(0, 100).ToString() + "\n";
+            t_line += rnd.Next(0, 100).ToString() + "," + rnd.Next(0, 100).ToString() + "\n";
+        }
+        
+        File.WriteAllText("../../../r_table.csv", r_line);
+        File.WriteAllText("../../../t_table.csv", t_line);
+    }
+}
